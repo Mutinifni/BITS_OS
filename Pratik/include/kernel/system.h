956bed6c30d9fe277e308899ea8c4648b61f9c5c
ps06756 <ps06756@gmail.com>
2014-09-20 01:02:39 +0530

    Final working code with non-recursinve output functions.

--- Pratik/include/kernel/system.h
+++ Pratik/include/kernel/system.h
@@ -1,9 +1,23 @@
-#ifndef SYSTEM_H 
-/* This header file will describe the prototypes we will use in setting up the header file. */ 
-#define SYSTEM_H 
-#define uint32_t unsigned int 
-void gdt_set_gate(int num, unsigned long base, unsigned long limit, unsigned char access, unsigned char gran) ; 
-// This will setup the descriptor in the gdt.
-void gdt_install() 
-// This will create the new gdt.  
-#endif 
+#ifndef SYSTEM_H
+#define SYSTEM_H
+#define size_t unsigned int 
+
+/* This defines what the stack looks like after an ISR was running */
+/*struct regs
+{
+    unsigned int gs, fs, es, ds;
+    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    unsigned int int_no, err_code;
+    unsigned int eip, cs, eflags, useresp, ss;    
+};*/
+
+
+void *memcpy(void *dest, const void *src, size_t count);
+void *memset(void *dest, char val, size_t count);
+unsigned short *memsetw(unsigned short *dest, unsigned short val, size_t count);
+size_t strlen(const char *str);
+unsigned char inportb (unsigned short _port);
+void outportb (unsigned short _port, unsigned char _data);
+
+		
+#endif
