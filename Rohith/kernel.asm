341f438a0d786fef4d8d0a6d30216cb64cd6846a
MadaraUchiha-314 <rohithr31@gmail.com>
2014-09-21 00:40:25 +0530

    Joining The Party

--- Rohith/kernel.asm
+++ Rohith/kernel.asm
@@ -1,79 +1,356 @@
+; This is the kernel's entry point. We could either call main here,
+; or we can use this to setup the stack or other nice stuff, like
+; perhaps setting up the GDT and segments. Please note that interrupts
+; are disabled at this point: More on interrupts later!
+[BITS 32]
+global start
+start:
+    mov esp, _sys_stack     ; This points the stack to our new stack area
+    jmp stublet
 
- 	;;  This is the nasm file.
-	;;  Link  with :- ld linker_script.ld kernel_main.o kernel.o -o myKernel -m elf_i386
-; Declare constants used for creating a multiboot header.
-MBALIGN     equ  1<<0                   ; align loaded modules on page boundaries
-MEMINFO     equ  1<<1                   ; provide memory map
-FLAGS       equ  MBALIGN | MEMINFO      ; this is the Multiboot 'flag' field
-MAGIC       equ  0x1BADB002             ; 'magic number' lets bootloader find the header
-CHECKSUM    equ -(MAGIC + FLAGS)        ; checksum of above, to prove we are multiboot
- 
-; Declare a header as in the Multiboot Standard. We put this into a special
-; section so we can force the header to be in the start of the final program.
-; You don't need to understand all these details as it is just magic values that
-; is documented in the multiboot standard. The bootloader will search for this
-; magic sequence and recognize us as a multiboot kernel.
-section .multiboot
-align 4
-	dd MAGIC
-	dd FLAGS
-	dd CHECKSUM
- 
-; Currently the stack pointer register (esp) points at anything and using it may
-; cause massive harm. Instead, we'll provide our own stack. We will allocate
-; room for a small temporary stack by creating a symbol at the bottom of it,
-; then allocating 16384 bytes for it, and finally creating a symbol at the top.
-section .bootstrap_stack
-align 4
-stack_bottom:
-times 16384 db 0
-stack_top:
- 
-; The linker script specifies _start as the entry point to the kernel and the
-; bootloader will jump to this position once the kernel has been loaded. It
-; doesn't make sense to return from this function as the bootloader is gone.
-section .text
-global _start
-_start:
-	; Welcome to kernel mode! We now have sufficient code for the bootloader to
-	; load and run our operating system. It doesn't do anything interesting yet.
-	; Perhaps we would like to call printf("Hello, World\n"). You should now
-	; realize one of the profound truths about kernel mode: There is nothing
-	; there unless you provide it yourself. There is no printf function. There
-	; is no <stdio.h> header. If you want a function, you will have to code it
-	; yourself. And that is one of the best things about kernel development:
-	; you get to make the entire system yourself. You have absolute and complete
-	; power over the machine, there are no security restrictions, no safe
-	; guards, no debugging mechanisms, there is nothing but what you build.
- 
-	; By now, you are perhaps tired of assembly language. You realize some
-	; things simply cannot be done in C, such as making the multiboot header in
-	; the right section and setting up the stack. However, you would like to
-	; write the operating system in a higher level language, such as C or C++.
-	; To that end, the next task is preparing the processor for execution of
-	; such code. C doesn't expect much at this point and we only need to set up
-	; a stack. Note that the processor is not fully initialized yet and stuff
-	; such as floating point instructions are not available yet.
- 
-	; To set up a stack, we simply set the esp register to point to the top of
-	; our stack (as it grows downwards).
-	mov esp, stack_top
- 
-	; We are now ready to actually execute C code. We cannot embed that in an
-	; assembly file, so we'll create a kernel.c file in a moment. In that file,
-	; we'll create a C entry point called kernel_main and call it here.
-	extern kernel_main
-	call kernel_main
- 
-	; In case the function returns, we'll want to put the computer into an
-	; infinite loop. To do that, we use the clear interrupt ('cli') instruction
-	; to disable interrupts, the halt instruction ('hlt') to stop the CPU until
-	; the next interrupt arrives, and jumping to the halt instruction if it ever
-	; continues execution, just to be safe.
-;; 	cli
-;; .hang:
-;; 	hlt
-;; 	jmp .hang
+; This part MUST be 4byte aligned, so we solve that issue using 'ALIGN 4'
+ALIGN 4
+mboot:
+    ; Multiboot macros to make a few lines later more readable
+    MULTIBOOT_PAGE_ALIGN    equ 1<<0
+    MULTIBOOT_MEMORY_INFO   equ 1<<1
+    MULTIBOOT_AOUT_KLUDGE   equ 1<<16
+    MULTIBOOT_HEADER_MAGIC  equ 0x1BADB002
+    MULTIBOOT_HEADER_FLAGS  equ MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEMORY_INFO | MULTIBOOT_AOUT_KLUDGE
+    MULTIBOOT_CHECKSUM  equ -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
+    EXTERN code, bss, end
 
-	;;  Above 4 lines are the commented lines as found in the original assembly file in the website wiki osdev.org
-jmp $ 
+    ; This is the GRUB Multiboot header. A boot signature
+    dd MULTIBOOT_HEADER_MAGIC
+    dd MULTIBOOT_HEADER_FLAGS
+    dd MULTIBOOT_CHECKSUM
+    
+    ; AOUT kludge - must be physical addresses. Make a note of these:
+    ; The linker script fills in the data for these ones!
+    dd mboot
+    dd code
+    dd bss
+    dd end
+    dd start
+
+; This is an endless loop here. Make a note of this: Later on, we
+; will insert an 'extern _main', followed by 'call _main', right
+; before the 'jmp $'.
+stublet:
+    extern kernel_main
+    call   kernel_main
+    jmp $
+
+; This will set up our new segment registers. We need to do
+; something special in order to set CS. We do what is called a
+; far jump. A jump that includes a segment as well as an offset.
+; This is declared in C as 'extern void gdt_flush();'
+global gdt_flush     ; Allows the C code to link to this
+extern gp            ; Says that '_gp' is in another file
+gdt_flush:
+    lgdt [gp]        ; Load the GDT with our 'gp' which is a special pointer
+    mov ax, 0x10      ; 0x10 is the offset in the GDT to our data segment
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    jmp 0x08:flush2   ; 0x08 is the offset to our code segment: Far jump!
+flush2:
+    ret               ; Returns back to the C code!
+
+; Loads the IDT defined in '_idtp' into the processor.
+; This is declared in C as 'extern void idt_load();'
+global idt_load
+extern idtp
+idt_load:
+    lidt [idtp]
+    ret
+
+; Interrupt Service Routines
+global isr0
+global isr1
+global isr2
+global isr3
+global isr4
+global isr5
+global isr6
+global isr7
+global isr8
+global isr9
+global isr10
+global isr11
+global isr12
+global isr13
+global isr14
+global isr15
+global isr16
+global isr17
+global isr18
+global isr19
+global isr20
+global isr21
+global isr22
+global isr23
+global isr24
+global isr25
+global isr26
+global isr27
+global isr28
+global isr29
+global isr30
+global isr31
+
+;  0: Divide By Zero Exception
+isr0:
+    cli
+    push byte 0
+    push byte 0
+    jmp isr_common_stub
+
+;  1: Debug Exception
+isr1:
+    cli
+    push byte 0
+    push byte 1
+    jmp isr_common_stub
+
+;  2: Non Maskable Interrupt Exception
+isr2:
+    cli
+    push byte 0
+    push byte 2
+    jmp isr_common_stub
+
+;  3: Int 3 Exception
+isr3:
+    cli
+    push byte 0
+    push byte 3
+    jmp isr_common_stub
+
+;  4: INTO Exception
+isr4:
+    cli
+    push byte 0
+    push byte 4
+    jmp isr_common_stub
+
+;  5: Out of Bounds Exception
+isr5:
+    cli
+    push byte 0
+    push byte 5
+    jmp isr_common_stub
+
+;  6: Invalid Opcode Exception
+isr6:
+    cli
+    push byte 0
+    push byte 6
+    jmp isr_common_stub
+
+;  7: Coprocessor Not Available Exception
+isr7:
+    cli
+    push byte 0
+    push byte 7
+    jmp isr_common_stub
+
+;  8: Double Fault Exception (With Error Code!)
+isr8:
+    cli
+    push byte 8
+    jmp isr_common_stub
+
+;  9: Coprocessor Segment Overrun Exception
+isr9:
+    cli
+    push byte 0
+    push byte 9
+    jmp isr_common_stub
+
+; 10: Bad TSS Exception (With Error Code!)
+isr10:
+    cli
+    push byte 10
+    jmp isr_common_stub
+
+; 11: Segment Not Present Exception (With Error Code!)
+isr11:
+    cli
+    push byte 11
+    jmp isr_common_stub
+
+; 12: Stack Fault Exception (With Error Code!)
+isr12:
+    cli
+    push byte 12
+    jmp isr_common_stub
+
+; 13: General Protection Fault Exception (With Error Code!)
+isr13:
+    cli
+    push byte 13
+    jmp isr_common_stub
+
+; 14: Page Fault Exception (With Error Code!)
+isr14:
+    cli
+    push byte 14
+    jmp isr_common_stub
+
+; 15: Reserved Exception
+isr15:
+    cli
+    push byte 0
+    push byte 15
+    jmp isr_common_stub
+
+; 16: Floating Point Exception
+isr16:
+    cli
+    push byte 0
+    push byte 16
+    jmp isr_common_stub
+
+; 17: Alignment Check Exception
+isr17:
+    cli
+    push byte 0
+    push byte 17
+    jmp isr_common_stub
+
+; 18: Machine Check Exception
+isr18:
+    cli
+    push byte 0
+    push byte 18
+    jmp isr_common_stub
+
+; 19: Reserved
+isr19:
+    cli
+    push byte 0
+    push byte 19
+    jmp isr_common_stub
+
+; 20: Reserved
+isr20:
+    cli
+    push byte 0
+    push byte 20
+    jmp isr_common_stub
+
+; 21: Reserved
+isr21:
+    cli
+    push byte 0
+    push byte 21
+    jmp isr_common_stub
+
+; 22: Reserved
+isr22:
+    cli
+    push byte 0
+    push byte 22
+    jmp isr_common_stub
+
+; 23: Reserved
+isr23:
+    cli
+    push byte 0
+    push byte 23
+    jmp isr_common_stub
+
+; 24: Reserved
+isr24:
+    cli
+    push byte 0
+    push byte 24
+    jmp isr_common_stub
+
+; 25: Reserved
+isr25:
+    cli
+    push byte 0
+    push byte 25
+    jmp isr_common_stub
+
+; 26: Reserved
+isr26:
+    cli
+    push byte 0
+    push byte 26
+    jmp isr_common_stub
+
+; 27: Reserved
+isr27:
+    cli
+    push byte 0
+    push byte 27
+    jmp isr_common_stub
+
+; 28: Reserved
+isr28:
+    cli
+    push byte 0
+    push byte 28
+    jmp isr_common_stub
+
+; 29: Reserved
+isr29:
+    cli
+    push byte 0
+    push byte 29
+    jmp isr_common_stub
+
+; 30: Reserved
+isr30:
+    cli
+    push byte 0
+    push byte 30
+    jmp isr_common_stub
+
+; 31: Reserved
+isr31:
+    cli
+    push byte 0
+    push byte 31
+    jmp isr_common_stub
+
+
+extern fault_handler
+
+isr_common_stub:
+    pusha
+    push ds
+    push es
+    push fs
+    push gs
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov eax, esp
+    push eax
+   ; mov eax, fault_handler
+   ; call eax
+   call fault_handler 
+    pop eax
+    pop gs
+    pop fs
+    pop es
+    pop ds
+    popa
+    add esp, 8
+    iret
+
+; Here is the definition of our BSS section. Right now, we'll use
+; it just to store the stack. Remember that a stack actually grows
+; downwards, so we declare the size of the data before declaring
+; the identifier '_sys_stack'
+SECTION .bss
+    resb 8192               ; This reserves 8KBytes of memory here
+_sys_stack:
---
+++ Rohith/lib/kernel/scrn.c
@@ -0,0 +1,220 @@
+/* This file defines the screen related functions in the operating system. */ 
+
+#include <system.h>
+#include <scrn.h> 
+/* These define our textpointer, our background and foreground
+*  colors (attributes), and x and y cursor coordinates */
+unsigned short *textmemptr;
+int attrib = 0x0F;
+int csr_x = 0, csr_y = 0;
+/* Scrolls the screen */
+void scroll(screen* ptr)
+{
+    unsigned blank, temp;
+
+    /* A blank is defined as a space... we need to give it
+    *  backcolor too */
+    blank = 0x20 | (ptr->attrib << 8);
+
+    /* Row 25 is the end, this means we need to scroll up */
+    if(ptr->csr_y >= 25)
+    {
+        /* Move the current text chunk that makes up the screen
+        *  back in the buffer by a line */
+        temp = ptr->csr_y - 25 + 1;
+        memcpy (ptr->textmemptr, ptr->textmemptr + temp * 80, (25 - temp) * 80 * 2);
+
+        /* Finally, we set the chunk of memory that occupies
+        *  the last line of text to our 'blank' character */
+        memsetw (ptr->textmemptr + (25 - temp) * 80, blank, 80);
+        ptr->csr_y = 25 - 1;
+    }
+}
+
+/* Updates the hardware cursor: the little blinking line
+*  on the screen under the last character pressed! */
+void move_csr(screen* ptr)
+{
+    unsigned temp;
+
+    /* The equation for finding the index in a linear
+    *  chunk of memory can be represented by:
+    *  Index = [(y * width) + x] */
+    temp = (ptr->csr_y) * 80 + (ptr->csr_x);
+
+    /* This sends a command to indicies 14 and 15 in the
+    *  CRT Control Register of the VGA controller. These
+    *  are the high and low bytes of the index that show
+    *  where the hardware cursor is to be 'blinking'. To
+    *  learn more, you should look up some VGA specific
+    *  programming documents. A great start to graphics:
+    *  http://www.brackeen.com/home/vga */
+    outportb(0x3D4, 14);
+    outportb(0x3D5, temp >> 8);
+    outportb(0x3D4, 15);
+    outportb(0x3D5, temp);
+}
+
+/* Clears the screen */
+void cls(screen* ptr)
+{
+    unsigned blank;
+    int i;
+
+    /* Again, we need the 'short' that will be used to
+    *  represent a space with color */
+    blank = 0x20 | (ptr->attrib << 8);
+
+    /* Sets the entire screen to spaces in our current
+    *  color */
+    for(i = 0; i < 25; i++)
+        memsetw ((ptr->textmemptr) + i * 80, blank, 80);
+
+    /* Update out virtual cursor, and then move the
+    *  hardware cursor */
+    (ptr->csr_x) = 0;
+    (ptr->csr_y) = 0;
+    move_csr(ptr);
+}
+
+/* Puts a single character on the screen */
+void putch(screen* ptr,  char c)
+{
+    unsigned short *where;
+    unsigned att = (ptr->attrib) << 8;
+
+    /* Handle a backspace, by moving the cursor back one space */
+    if(c == 0x08)
+    {
+        if((ptr->csr_x) != 0) (ptr->csr_x)--;
+    }
+    /* Handles a tab by incrementing the cursor's x, but only
+    *  to a point that will make it divisible by 8 */
+    else if(c == 0x09)
+    {
+        (ptr->csr_x) = ((ptr->csr_x) + 8) & ~(8 - 1);
+    }
+    /* Handles a 'Carriage Return', which simply brings the
+    *  cursor back to the margin */
+    else if(c == '\r')
+    {
+        (ptr->csr_x) = 0;
+    }
+    /* We handle our newlines the way DOS and the BIOS do: we
+    *  treat it as if a 'CR' was also there, so we bring the
+    *  cursor to the margin and we increment the 'y' value */
+    else if(c == '\n')
+    {
+        (ptr->csr_x) = 0;
+        (ptr->csr_y)++;
+    }
+    /* Any character greater than and including a space, is a
+    *  printable character. The equation for finding the index
+    *  in a linear chunk of memory can be represented by:
+    *  Index = [(y * width) + x] */
+    else if(c >= ' ')
+    {
+        where = ptr->textmemptr + ((ptr->csr_y) * 80 + (ptr->csr_x));
+        *where = c | att;	/* Character AND attributes: color */
+        (ptr->csr_x)++;
+    }
+
+    /* If the cursor has reached the edge of the screen's width, we
+    *  insert a new line in there */
+    if((ptr->csr_x) >= 80)
+    {
+        (ptr->csr_x) = 0;
+        (ptr->csr_y)++;
+    }
+
+    /* Scroll the screen if needed, and finally move the cursor */
+    scroll(ptr);
+    move_csr(ptr);
+}
+
+/* Uses the above routine to output a string... */
+void puts(screen* ptr, const char *text)
+{
+    for (unsigned int i = 0; i < strlen(text); i++)
+    {
+        putch(ptr, text[i]);
+    }
+}
+
+/* Sets the forecolor and backcolor that we will use */
+void settextcolor(screen* ptr, unsigned char forecolor, unsigned char backcolor)
+{
+    /* Top 4 bytes are the background, bottom 4 bytes
+    *  are the foreground color */
+    (ptr->attrib) = (backcolor << 4) | (forecolor & 0x0F) ;
+}
+
+/* Sets our text-mode VGA pointer, then clears the screen for us */
+void init_video(screen* ptr)
+{
+    initialize_screen(ptr) ; 
+    (ptr->textmemptr) = (unsigned short *)0xB8000;
+    cls(ptr);
+}
+void initialize_screen(screen* ptr)
+{
+    // We will now initialize the screen here.
+    ptr->attrib = 0x0F;
+    ptr->csr_x = 0 ; 
+    ptr->csr_y = 0  ; 
+}
+void print_text(screen* ptr, const char *text)
+{
+    size_t i;
+
+    for (i = 0; i < strlen(text); i++)
+    {
+        putch(ptr, text[i]);
+    }
+}
+
+void print_int(screen* ptr, int n)
+{
+    if(n < 0)
+    {
+        putch(ptr, '-');
+        n = -n;
+    }
+    if(n/10)
+    {
+        print_int(ptr, n/10);
+    }
+    putch(ptr, n%10 + '0');
+}
+
+void print_long_int(screen* ptr, long int n)
+{
+    if(n < 0)
+    {
+        putch(ptr, '-');
+        n = -n;
+    }
+    if(n/10)
+    {
+        print_long_int(ptr, n/10);
+    }
+    putch(ptr, n%10 + '0');
+}
+
+void print_double(screen* ptr, double n)
+{
+    print_int(ptr, (int) n);
+    n -= (int) n;
+    putch(ptr,'.');
+    n *= 1000000;
+    print_int(ptr, (int) n);
+}
+
+void print_float(screen* ptr, float n)
+{
+    print_int(ptr, (int) n);
+    n -= (int) n;
+    putch(ptr,'.');
+    n *= 1000000;
+    print_int(ptr, (int) n);
+}
