74b09fbf0810bd9afdd7dfd7ea5667f34065b503
ps06756 <ps06756@gmail.com>
2014-09-23 15:06:44 +0530

    Implemented partial exception handling mechanism.

---
+++ BITS_OS/lib/exception.c
@@ -0,0 +1,30 @@
+#include <exception.h> 
+#include <processor.h>
+#include <io.h>
+short exception_nested = 0 ;  
+void divide_by_zero(breg val)
+{
+  exception_nested++ ; 
+  //disable_interrupts() ; 
+  print("Divide by zero exception raised..\n") ;
+  print(val.esp) ;
+  print("\n") ; 
+  if(exception_nested > 5)
+    {
+      for(;;) ; 
+    }
+  //enable_interrupts() ; 
+}
+void double_fault(breg val)
+{
+  //disable_interrupts() ;
+  exception_nested++ ; 
+  print("Double fault occurred..\n") ;
+  print(val.esp) ; 
+  print("\n") ;
+  if(exception_nested > 5)
+    {
+      for(;;) ;
+    }
+  //enable_interrupts() ; 
+}
